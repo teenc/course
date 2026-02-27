ccda0a081e671947986b08cec10e88934390bc6b
teenc <skellycantsleep@gmail.com>
2026-02-28 01:02:14 +0200

    repushing

--- 01-variables.js
+++ 01-variables.js
@@ -3,23 +3,25 @@
 // ---------
 
 // "const" is used to declare a variable that cannot be reassigned
-const name = "lilo";
+const name1 = "lilo";
 const age1 = 16;
 
 // trying to change a const variable causes an error
-age1 = 17; 
+age1 = 17;
+name1 = "akra";
 
-console.log(age1); 
+console.log(`my name is ${name1} and i am: `, age1);
 // won't run because the error above stops execution
 
 
 
 // "let" is used to declare a variable that CAN be reassigned later
-
+let name2 = "lilo";
 let age2 = 13;
 
 // reassigning a let variable is allowed
 age2 = 14;
+name2 = "akra";
 
-console.log(age2); 
-// prints 14 because we reassigned the variable
+console.log(`my name is ${name2}and i am: `, age2); 
+// output: my name is akra and i am: 14 (because we reassigned the variable)
